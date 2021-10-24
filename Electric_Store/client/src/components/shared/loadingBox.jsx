80097aabc10d745b1970deb408aaa176fc8c2145
Gal <galpalas89@gmail.com>
2021-10-24 19:48:15 +0300

    install ESLint and add more style to showcase

--- Electric_Store/client/src/components/shared/loadingBox.jsx
+++ Electric_Store/client/src/components/shared/loadingBox.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 const LoadingBox = () => {
   return (
-    <div className="p-5 mt-4">
-      <h5>
-        <i className="fa fa-spinner fa-spin "></i> Loading...
-      </h5>
+    <div className="text-center p-5">
+      <div
+        className="spinner-border text-warning"
+        style={{ width: "3rem", height: "3rem" }}
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+      <div>
+        <strong>Loading...</strong>
+      </div>
     </div>
   );
 };
