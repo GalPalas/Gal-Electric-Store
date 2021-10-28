d2ed2fa639233bfa224d97d7edff226997131f17
Gal <galpalas89@gmail.com>
2021-10-28 15:35:34 +0300

    Create Sample Users In MongoDB

--- Electric_Store/server/data.js
+++ Electric_Store/server/data.js
@@ -1,4 +1,19 @@
+import bcrypt from "bcryptjs";
 const data = {
+  users: [
+    {
+      name: "Gal",
+      email: "gal123@gmail.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "Dan",
+      email: "dan@gmail.com",
+      password: bcrypt.hashSync("4567", 8),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       _id: "1",
